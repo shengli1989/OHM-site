de0af9d21005ada9c25011d2a43832e4765e2201
shengli1989 <shengli@evendesign.tw>
2013-04-12 17:14:57 +0800

    初始化設定

--- config.rb
+++ config.rb
@@ -1,21 +1,20 @@
+# Configuring Slim output style
+require 'slim'
+Slim::Engine.set_default_options :pretty => true, :disable_escape => true, :format => :html5
+
 # Require any additional compass plugins here.
 
-# Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
 output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
 line_comments = false
 
 # If you prefer the indented syntax, you might want to regenerate this
@@ -24,3 +23,7 @@ line_comments = false
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 preferred_syntax = :scss
+
+
+line_comments = false # by Fire.app
+output_style = :nested # by Fire.app
